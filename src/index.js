7a3de53ac34ba0b0204c9b99f1a0cad8755cbefb
蒋玮乘 <jiangweicheng001@lianjia.com>
2021-04-19 17:04:44 +0800

    feat: 支持自定义后缀解析

--- src/index.js
+++ src/index.js
@@ -1,43 +1,49 @@
-/* eslint-disable */
 const parse = require('@babel/parser').parse;
 const traverse = require('@babel/traverse').default;
 const generate = require('@babel/generator').default;
 const Visitor = require('./visitor');
 const loaderUtils = require('loader-utils');
 
+const defaultExtensionTest = /\.module\.(less|scss|css)$/;
 
 module.exports = function (content) {
-    const resource = this.resourcePath;
-    const options = loaderUtils.getOptions(this);
-    const visitor = new Visitor();
+  const resource = this.resourcePath;
+  const options = loaderUtils.getOptions(this) || {};
+  const visitor = new Visitor({
+    extensionTest: options.extensionTest || defaultExtensionTest,
+    excludeExtensionTest: null,
+  });
 
-    if (options && options.test) {
-        if (options.test(resource) === false) {
-            return;
-        }
-    } else if (/\/node_modules\//g.test(resource)) {
-        return content;
+  if (options && options.test) {
+    if (options.test(resource) === false) {
+      return;
     }
+  } else if (/\/node_modules\//g.test(resource)) {
+    return content;
+  }
 
-    const ast = parse(content, {
-        sourceType: "module",
-        sourceFilename: resource,
-        plugins: [
-            "jsx",
-            "decorators-legacy",
-            "classProperties",
-            "objectRestSpread",
-            "asyncGenerators",
-            "typescript",
-            "dynamicImport"
-        ]
-    });
+  const ast = parse(content, {
+    sourceType: 'module',
+    sourceFilename: resource,
+    plugins: [
+      'jsx',
+      'decorators-legacy',
+      'classProperties',
+      'objectRestSpread',
+      'asyncGenerators',
+      'typescript',
+      'dynamicImport',
+    ],
+  });
 
-    traverse(ast, visitor.visitor());
-    const transformContent = generate(ast, { sourceMaps: true }, { [resource]: content });
+  traverse(ast, visitor.visitor());
+  const transformContent = generate(
+    ast,
+    { sourceMaps: true },
+    { [resource]: content }
+  );
 
-    this.callback(null, transformContent.code, transformContent.map);
-}
+  this.callback(null, transformContent.code, transformContent.map);
+};
 
 module.exports.raw = false;
-
--- src/visitor.js
+++ src/visitor.js
@@ -1,291 +1,277 @@
 const babelTypes = require('@babel/types');
 
 class ParseVisitor {
-    constructor(options = {}) {
-        this.options = options;
-        this.styleNames = [];
-        this.wrapFunctionIdentifier = undefined;
-        this.callFuncIdentifier = undefined;
+  constructor(options = {}) {
+    this.options = options;
+    this.styleNames = [];
+    this.wrapFunctionIdentifier = undefined;
+    this.callFuncIdentifier = undefined;
+  }
+
+  visitor() {
+    return {
+      Program: {
+        enter: this.enter.bind(this),
+        exit: this.exit.bind(this),
+      },
+      JSXAttribute: this.JSXAttributeVisitor.bind(this),
+      ImportDeclaration: this.ImportDeclarationVisitor.bind(this),
+    };
+  }
+
+  enter(path) {
+    this.wrapFunctionIdentifier = path.scope.generateUidIdentifier(
+      'warpFunction'
+    );
+    this.callFuncIdentifier = path.scope.generateUidIdentifier('callFunction');
+  }
+
+  exit(path) {
+    const func = this.createwrapFunction();
+    const callFunc = this.createCallExpression();
+    path.node.body.push(func);
+    path.node.body.push(callFunc);
+  }
+
+  // JSXAttribute 访问者
+  JSXAttributeVisitor(path) {
+    if (path.node.name.name !== 'className') return;
+    if (path.node.value === null) return;
+    // 没获取到倒入的less或css模块，取消后续操作
+    if (this.styleNames.length === 0) return;
+
+    let classNames;
+    const expr = path.node.value;
+    if (babelTypes.isJSXExpressionContainer(expr)) {
+      // 支持动态调用
+      expr.expression = babelTypes.CallExpression(
+        babelTypes.Identifier(this.callFuncIdentifier.name),
+        [expr.expression]
+      );
+    } else if (babelTypes.isStringLiteral(path.node.value)) {
+      classNames = this.split(path.node.value.value);
     }
 
-    visitor() {
-        return {
-            Program: {
-                enter: this.enter.bind(this),
-                exit: this.exit.bind(this)
-            },
-            JSXAttribute: this.JSXAttributeVisitor.bind(this),
-            ImportDeclaration: this.ImportDeclarationVisitor.bind(this),
-        }
+    if (classNames && classNames.length !== 0) {
+      path.node.value = this.getCorrectRefExpression(classNames);
+    }
+  }
+
+  // ImportDeclaration 访问者
+  ImportDeclarationVisitor(path) {
+    const libName = path.node.source.value;
+    const importTypes = path.node.specifiers;
+
+    if (
+      this.options.excludeExtensionTest &&
+      this.options.excludeExtensionTest.test(libName)
+    ) {
+      return;
     }
 
-    enter(path) {
-        this.wrapFunctionIdentifier = path.scope.generateUidIdentifier('warpFunction');
-        this.callFuncIdentifier = path.scope.generateUidIdentifier('callFunction');
+    if (!this.options.extensionTest.test(libName)) {
+      return;
     }
 
-    exit(path) {
-        const func = this.createwrapFunction();
-        const callFunc = this.createCallExpression();
-        path.node.body.push(func);
-        path.node.body.push(callFunc);
+    if (importTypes.length === 0) {
+      // 使用默认引入模式
+      const nameIdentifier = path.scope.generateUidIdentifier('styles');
+      importTypes.push(babelTypes.ImportDefaultSpecifier(nameIdentifier));
+
+      this.styleNames.push(nameIdentifier.name);
+      return;
     }
 
-    // JSXAttribute 访问者
-    JSXAttributeVisitor(path) {
-        if (path.node.name.name !== 'className') return;
-        if (path.node.value === null) return;
-        // 没获取到倒入的less或css模块，取消后续操作
-        if (this.styleNames.length === 0) return;
+    if (this.isImportSpecifier(importTypes)) {
+      importTypes.forEach((specifier) =>
+        this.styleNames.push([specifier.local.name])
+      );
 
-        let classNames;
-        const expr = path.node.value;
-        if (babelTypes.isJSXExpressionContainer(expr)) {
-
-            // 支持动态调用
-            expr.expression = babelTypes.CallExpression(
-                babelTypes.Identifier(this.callFuncIdentifier.name),
-                [expr.expression]
-            );
-        }
-
-        else if (babelTypes.isStringLiteral(path.node.value)) {
-            classNames = this.split(path.node.value.value)
-        }
-
-        if (classNames && classNames.length !== 0) {
-            path.node.value = this.getCorrectRefExpression(classNames);
-        }
+      return;
     }
 
-    // ImportDeclaration 访问者
-    ImportDeclarationVisitor(path) {
-        const libName = path.node.source.value;
-        const importTypes = path.node.specifiers;
+    if (
+      !babelTypes.isImportDefaultSpecifier(importTypes[0]) &&
+      !babelTypes.isImportNamespaceSpecifier(importTypes[0])
+    ) {
+      // const errMsg = codeFrameColumns(content, { ...importTypes[0].loc }, {
+      //     message: "'css/less/scss'引入必须采用默认的引入方式！eg: import styles from 'index.less'"
+      // });
 
-        if (
-            [
-                '.module.less',
-                '.module.scss',
-                '.module.css'
-            ].every(_ => !libName.endsWith(_))
-        ) return;
+      // throw new Error(errMsg);
 
-        if (importTypes.length === 0) {
-            // 使用默认引入模式
-            const nameIdentifier = path.scope.generateUidIdentifier("styles");
-            importTypes.push(babelTypes.ImportDefaultSpecifier(nameIdentifier));
-
-            this.styleNames.push(nameIdentifier.name);
-            return;
-        }
-
-        if (this.isImportSpecifier(importTypes)) {
-            importTypes.forEach(
-                specifier => this.styleNames.push([
-                    specifier.local.name
-                ])
-            );
-
-            return;
-        }
-
-        if (
-            !babelTypes.isImportDefaultSpecifier(importTypes[0]) &&
-            !babelTypes.isImportNamespaceSpecifier(importTypes[0])
-        ) {
-            // const errMsg = codeFrameColumns(content, { ...importTypes[0].loc }, {
-            //     message: "'css/less/scss'引入必须采用默认的引入方式！eg: import styles from 'index.less'"
-            // });
-
-            // throw new Error(errMsg);
-
-            return;
-        }
-
-        this.styleNames.push(importTypes[0].local.name);
+      return;
     }
 
-    getCorrectRefExpression(arrayString) {
+    this.styleNames.push(importTypes[0].local.name);
+  }
 
-        // 此处无需校验 arrayString
-        if (arrayString.length === 1) {
-            const className = arrayString[0];
+  getCorrectRefExpression(arrayString) {
+    // 此处无需校验 arrayString
+    if (arrayString.length === 1) {
+      const className = arrayString[0];
 
-            return babelTypes.JSXExpressionContainer(
-                this.getRefExpression(className)
-            );
-        }
+      return babelTypes.JSXExpressionContainer(
+        this.getRefExpression(className)
+      );
+    }
 
-        const expression = [];
-        const quasis = [babelTypes.TemplateElement({ raw: "", cooked: "" })];
+    const expression = [];
+    const quasis = [babelTypes.TemplateElement({ raw: '', cooked: '' })];
 
-        arrayString.forEach((className, index) => {
-            let quasisItem;
+    arrayString.forEach((className, index) => {
+      let quasisItem;
 
-            const expressionItem = this.getRefExpression(className);
+      const expressionItem = this.getRefExpression(className);
 
-            if (index === arrayString.length - 1) {
-                quasisItem = babelTypes.TemplateElement({ raw: "", cooked: "" });
-            } else {
-                quasisItem = babelTypes.TemplateElement({ raw: " ", cooked: " " });
-            }
+      if (index === arrayString.length - 1) {
+        quasisItem = babelTypes.TemplateElement({ raw: '', cooked: '' });
+      } else {
+        quasisItem = babelTypes.TemplateElement({ raw: ' ', cooked: ' ' });
+      }
 
-            expression.push(expressionItem);
-            quasis.push(quasisItem);
-        });
+      expression.push(expressionItem);
+      quasis.push(quasisItem);
+    });
 
-        return babelTypes.JSXExpressionContainer(
-            babelTypes.TemplateLiteral(quasis, expression)
+    return babelTypes.JSXExpressionContainer(
+      babelTypes.TemplateLiteral(quasis, expression)
+    );
+  }
+
+  isImportSpecifier(specifiers) {
+    return specifiers.every(babelTypes.isImportSpecifier);
+  }
+
+  split(value) {
+    return value.split(' ').filter((_) => _ !== '');
+  }
+
+  getRefExpression(className) {
+    const funcName = this.wrapFunctionIdentifier.name;
+    return babelTypes.CallExpression(babelTypes.Identifier(funcName), [
+      babelTypes.StringLiteral(className),
+    ]);
+  }
+
+  // 根据styleNames创建包裹函数
+  createwrapFunction() {
+    const funcName = this.wrapFunctionIdentifier.name;
+    const paramName = 'className';
+    const body = [];
+
+    this.styleNames.reverse().forEach((styleName) => {
+      let ifstatement;
+
+      if (Array.isArray(styleName)) {
+        const name = styleName[0];
+
+        ifstatement = babelTypes.IfStatement(
+          babelTypes.BinaryExpression(
+            '===',
+            babelTypes.Identifier(name),
+            babelTypes.Identifier(paramName)
+          ),
+          babelTypes.ReturnStatement(babelTypes.Identifier(paramName))
         );
-    }
-
-    isImportSpecifier(specifiers) {
-        return specifiers.every(babelTypes.isImportSpecifier);
-    }
-
-    split(value) {
-        return value.split(' ').filter(_ => _ !== '');
-    }
-
-    getRefExpression(className) {
-        const funcName = this.wrapFunctionIdentifier.name;
-        return babelTypes.CallExpression(
-            babelTypes.Identifier(funcName),
-            [babelTypes.StringLiteral(className)]
-        );
-    }
-
-    // 根据styleNames创建包裹函数
-    createwrapFunction() {
-        const funcName = this.wrapFunctionIdentifier.name;
-        const paramName = 'className';
-        const body = [];
-
-        this.styleNames.reverse().forEach(styleName => {
-            let ifstatement;
-
-            if (Array.isArray(styleName)) {
-                const name = styleName[0];
-
-                ifstatement = babelTypes.IfStatement(
-                    babelTypes.BinaryExpression(
-                        '===',
-                        babelTypes.Identifier(name),
-                        babelTypes.Identifier(paramName)
-                    ),
-                    babelTypes.ReturnStatement(
-                        babelTypes.Identifier(paramName)
-                    )
-                );
-            } else {
-                ifstatement = babelTypes.IfStatement(
-                    babelTypes.MemberExpression(
-                        babelTypes.Identifier(styleName),
-                        babelTypes.Identifier(paramName),
-                        true
-                    ),
-                    babelTypes.ReturnStatement(
-                        babelTypes.MemberExpression(
-                            babelTypes.Identifier(styleName),
-                            babelTypes.Identifier(paramName),
-                            true
-                        )
-                    )
-                )
-            }
-
-            body.push(ifstatement);
-
-        });
-
-        body.push(
-            babelTypes.ReturnStatement(
-                babelTypes.Identifier(paramName),
+      } else {
+        ifstatement = babelTypes.IfStatement(
+          babelTypes.MemberExpression(
+            babelTypes.Identifier(styleName),
+            babelTypes.Identifier(paramName),
+            true
+          ),
+          babelTypes.ReturnStatement(
+            babelTypes.MemberExpression(
+              babelTypes.Identifier(styleName),
+              babelTypes.Identifier(paramName),
+              true
             )
+          )
         );
+      }
 
-        return babelTypes.FunctionDeclaration(
-            babelTypes.Identifier(funcName),
-            [babelTypes.Identifier(paramName)],
-            babelTypes.BlockStatement(body)
-        );
-    }
+      body.push(ifstatement);
+    });
 
-    createCallExpression() {
-        const funcName = this.callFuncIdentifier.name;
-        const paramName = 'result';
-        const classNames = 'classNames';
-        const body = [];
+    body.push(babelTypes.ReturnStatement(babelTypes.Identifier(paramName)));
 
-        const ifStatement = babelTypes.ifStatement(
-            babelTypes.BinaryExpression(
-                '!==',
-                babelTypes.UnaryExpression(
-                    'typeof',
-                    babelTypes.Identifier(paramName)
-                ),
-                babelTypes.StringLiteral('string')
-            ),
-            babelTypes.ReturnStatement(
-                babelTypes.Identifier(paramName)
-            )
-        );
+    return babelTypes.FunctionDeclaration(
+      babelTypes.Identifier(funcName),
+      [babelTypes.Identifier(paramName)],
+      babelTypes.BlockStatement(body)
+    );
+  }
 
-        const statement1 = babelTypes.VariableDeclaration(
-            'const',
-            [babelTypes.VariableDeclarator(
-                babelTypes.Identifier(classNames),
-                babelTypes.CallExpression(
-                    babelTypes.MemberExpression(
-                        babelTypes.CallExpression(
-                            babelTypes.MemberExpression(
-                                babelTypes.Identifier(paramName),
-                                babelTypes.Identifier('split')
-                            ),
-                            [babelTypes.StringLiteral(' ')]
-                        ),
-                        babelTypes.Identifier('filter')
-                    ),
-                    [babelTypes.ArrowFunctionExpression(
-                        [babelTypes.Identifier('_')],
-                        babelTypes.BinaryExpression(
-                            '!==',
-                            babelTypes.Identifier('_'),
-                            babelTypes.StringLiteral('')
-                        )
-                    )]
-                )
-            )]
-        );
+  createCallExpression() {
+    const funcName = this.callFuncIdentifier.name;
+    const paramName = 'result';
+    const classNames = 'classNames';
+    const body = [];
 
-        const statement2 = babelTypes.ReturnStatement(
+    const ifStatement = babelTypes.ifStatement(
+      babelTypes.BinaryExpression(
+        '!==',
+        babelTypes.UnaryExpression('typeof', babelTypes.Identifier(paramName)),
+        babelTypes.StringLiteral('string')
+      ),
+      babelTypes.ReturnStatement(babelTypes.Identifier(paramName))
+    );
+
+    const statement1 = babelTypes.VariableDeclaration('const', [
+      babelTypes.VariableDeclarator(
+        babelTypes.Identifier(classNames),
+        babelTypes.CallExpression(
+          babelTypes.MemberExpression(
             babelTypes.CallExpression(
-                babelTypes.MemberExpression(
-                    babelTypes.CallExpression(
-                        babelTypes.MemberExpression(
-                            babelTypes.Identifier(classNames),
-                            babelTypes.Identifier('map')
-                        ),
-                        [babelTypes.Identifier(this.wrapFunctionIdentifier.name)]
-                    ),
-                    babelTypes.Identifier('join')
-                ),
-                [babelTypes.StringLiteral(' ')]
-            )
-        );
+              babelTypes.MemberExpression(
+                babelTypes.Identifier(paramName),
+                babelTypes.Identifier('split')
+              ),
+              [babelTypes.StringLiteral(' ')]
+            ),
+            babelTypes.Identifier('filter')
+          ),
+          [
+            babelTypes.ArrowFunctionExpression(
+              [babelTypes.Identifier('_')],
+              babelTypes.BinaryExpression(
+                '!==',
+                babelTypes.Identifier('_'),
+                babelTypes.StringLiteral('')
+              )
+            ),
+          ]
+        )
+      ),
+    ]);
 
-        body.push(ifStatement);
-        body.push(statement1);
-        body.push(statement2);
+    const statement2 = babelTypes.ReturnStatement(
+      babelTypes.CallExpression(
+        babelTypes.MemberExpression(
+          babelTypes.CallExpression(
+            babelTypes.MemberExpression(
+              babelTypes.Identifier(classNames),
+              babelTypes.Identifier('map')
+            ),
+            [babelTypes.Identifier(this.wrapFunctionIdentifier.name)]
+          ),
+          babelTypes.Identifier('join')
+        ),
+        [babelTypes.StringLiteral(' ')]
+      )
+    );
 
-        return babelTypes.FunctionDeclaration(
-            babelTypes.Identifier(funcName),
-            [babelTypes.Identifier(paramName)],
-            babelTypes.BlockStatement(body)
-        );
-    }
+    body.push(ifStatement);
+    body.push(statement1);
+    body.push(statement2);
 
+    return babelTypes.FunctionDeclaration(
+      babelTypes.Identifier(funcName),
+      [babelTypes.Identifier(paramName)],
+      babelTypes.BlockStatement(body)
+    );
+  }
 }
 
 module.exports = ParseVisitor;
